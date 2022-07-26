4333d6f72f869deb2bd4127819ed8135b2cd728b
chris-hildebrandt <ch.hild15@gmail.com>
2022-07-26 10:19:53 -0600

    comment-out template fluff

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 import { ValuesController } from "./Controllers/ValuesController.js";
 
 class App {
-  valuesController = new ValuesController();
+  // valuesController = new ValuesController();
+  // comment this out if you are not using it
 }
 
 window["app"] = new App();
